a13b8619ee1ad2957fc55f0c0666d1d18a66aa5b
Decade-qiu <3237550600@qq.com>
2022-11-19 08:59:01 +0800

    11/19

--- vHLL.cpp
+++ vHLL.cpp
@@ -20,7 +20,7 @@ public:
     int b_;
     int m_;
     int N_;
-    uint32_t SEED = 313;
+    uint32_t SEED = 131;
 
     vHLL(int N, int b) {
         b_ = b;
@@ -58,19 +58,23 @@ public:
             sum += 1.0 / (1 << ms_[index]);
             if (ms_[index] == 0) zero++;
         }
-        double ns = 1.0 * alpha(m_) * m_ * m_ / sum;
-        if (ns < 2.5 * m_ && zero != 0) {
-            ns = -m_*log2(zero / m_);
-        }
+        double ave = m_ / sum;
+        //ave = 0.008661963939488782 * ave * ave + 1.2152514269450094 * ave - 0.21991023804331547;
+        double ns = 1.0 * alpha(m_) * m_ * ave;
         double nf = (1.0 * N_ * m_ / (N_ - m_)) * (ns / m_ - n / N_);
-        vector<double> ret = {m_ / sum, nf};
+        vector<double> ret = {m_ / sum, round(nf+0.5)};
         return ret;
     }
 
     double getTotal() {
-        double sum = 0;
-        for (int i = 0; i < N_; i++) sum += 1.0/(1 << ms_[i]);
-        return 1.0 * alpha(N_) * N_ * N_ / sum;
+        double sum = 0, zero = 0;
+        for (int i = 0; i < N_; i++) {
+            sum += 1.0 / (1 << ms_[i]);
+        }
+        double ave = N_ / sum;
+        //ave = 0.008661963939488782 * ave * ave + 1.2152514269450094 * ave -0.21991023804331547;
+        double ns = 1.0 * alpha(N_) * N_ * ave;
+        return ns;
     }
 
     double alpha(int t) const {
@@ -119,11 +123,10 @@ public:
     }
 };
 
-void driver(int N, int b, int P) {
+void driver(int N, int b, int P, string res_data) {
     ifstream inf;
     ofstream ouf;
     string pkt_data = "E://DeskTop//train";
-    string res_data = "E://DeskTop//res";
     const char* src;
     const char* dst;
     string buf, t1, t2;
@@ -132,13 +135,10 @@ void driver(int N, int b, int P) {
     for (auto& p : filesystem::directory_iterator(pkt_data)) {
         vHLL hll = vHLL(N, b);
         inf.open(p.path().string(), ios::in);
-        /*ouf.open(res_data + "//size" + idx + ".txt"); idx++;*/
+        //ouf.open(res_data + "//size" + idx + ".txt"); idx++;
         ouf.open(res_data + "//"+to_string(P) + "ret" + idx + ".txt"); idx++;
         unordered_map<string, unordered_set<string>> flow;
         int sample = 0;
-        LARGE_INTEGER tt1, tt2, tc;
-        QueryPerformanceFrequency(&tc);
-        QueryPerformanceCounter(&tt1);
         while (getline(inf, buf)) {
             if (sample == 100) sample = 0;
             for (int i = 0; i < buf.size(); i++) {
@@ -153,11 +153,10 @@ void driver(int N, int b, int P) {
             flow[src].insert(dst);
             if (sample++ < P) hll.insert(src, dst);
         }
-        QueryPerformanceCounter(&tt2);
-        runt += (double)(tt2.QuadPart - tt1.QuadPart) / (double)tc.QuadPart;
         cout << flow.size() << endl;
         double n = hll.getTotal();
         double em = 0;
+        cout << n << endl;
         size_t am = 0;
         for (auto& cur : flow) {
             string f = cur.first;
@@ -166,26 +165,73 @@ void driver(int N, int b, int P) {
             double ave = esi[0], nf = esi[1];
             am = max(am, ss.size());
             em = max(em, nf);
-            //ouf << ss.size() << " " << esi << endl;
-            ouf << ss.size() << " " << nf << " "
-                << ave << " " << (double)P/100 << endl;
+            //ouf << ss.size() << " " << nf << endl;
+            ouf << ss.size() << " " << nf << " " << ave << " " << (double)P/100 << endl;
         }
         cout << am << " " << em << endl;
         inf.close();
         ouf.close();
     }
-    cout << "运行时间"  << endl;
+    cout << "Completed!"  << endl;
+}
+
+void Throughout(int N, int b, int P) {
+    ifstream inf;
+    ofstream ouf;
+    string pkt_data = "E://DeskTop//train";
+    const char* src;
+    const char* dst;
+    string buf, t1, t2;
+    double runt = 0;
+    int sample = 0, flowNum = 0;
+    for (auto& p : filesystem::directory_iterator(pkt_data)) {
+        vHLL hll = vHLL(N, b);
+        inf.open(p.path().string(), ios::in);
+        while (getline(inf, buf)) {
+            flowNum++;
+            if (sample == 100) sample = 0;
+            for (int i = 0; i < buf.size(); i++) {
+                if (buf[i] == ' ') {
+                    t1 = buf.substr(0, i);
+                    t2 = buf.substr(i + 1);
+                    break;
+                }
+            }
+            src = t1.c_str();
+            dst = t2.c_str();
+            auto start = std::chrono::system_clock::now();
+            if (sample++ < P) hll.insert(src, dst);
+            auto end = std::chrono::system_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+            runt += (double)(duration.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den;
+        }
+        inf.close();
+    }
+    cout << "pkt_num:" << flowNum << " "
+        "sampling_rate:" << 1.0 * P / 100 << " "
+        "vHLL_insert_time:" << runt << endl;
 }
 
 int main() {
-   /* vHLL hll = vHLL(1677722, 5);
+    /*vHLL hll = vHLL(1677722, 5);
     const char *ip = "10.23.44.1";
     const char *d = "192.111.12.10";
     hll.insert(ip, d);
     double n = hll.getTotal();
     cout << n << endl;
-    cout << hll.estimate(ip, n) << endl;*/
-    driver(1677722, 5, 10);
+    cout << hll.estimate(ip, n)[1] << endl;
+    auto start = std::chrono::system_clock::now();
+    if (n == 0) n++;
+    auto end = std::chrono::system_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    cout << (double)(duration.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den;*/
+    int p[5] = {10, 30, 50, 80, 100};
+    string r1 = "E://DeskTop//res";
+    string r2 = "E://DeskTop//res//base";
+    for (int i = 3; i < 4; i++) {
+        //Throughout(1677722, 5, P);
+        driver(1677722, 5, p[i], r1);
+    }
     /*for (int i = 0; i < 100; i++) {
         uint32_t res;
         MurmurHash3_x86_32(ip, 32, 313, &res);
